e33c3bec276d360cce50f3a26392c7d2b52de604
Michael Yung <mengyung@uw.edu>
2022-12-03 10:58:28 -0800

    set up

---
+++ hackathon/src/components/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        RETHINK
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
